860ce39295d289d2cab4183c53888213c14236db
atom-marcel <marcelmaluta@gmail.com>
2025-11-13 19:51:10 +0100

    TableView aktualisiert und nach gewünschtem Verhalten aktualisiert

--- Notenmanager/ExamsView.Designer.cs
+++ Notenmanager/ExamsView.Designer.cs
@@ -16,7 +16,6 @@ namespace Notenmanager
         private void InitializeComponent()
         {
             this.main = new TableView();
-            TableView sec = new TableView();
             DataTable dt = new DataTable();
 
             dt.Columns.Add("Klausurname");
@@ -30,21 +29,57 @@ namespace Notenmanager
                 dt.Rows.Add(e.Name, e.Percent.ToString(), e.Subject, e.LearningField, e.Date.ToString("dd.MM.yy"));
             }
             
-            main.Table = dt;
             main.Style.AlwaysShowHeaders = true;
             main.AutoSize = true;
-            main.ForceValidatePosDim = true;
+            main.FullRowSelect = true;
+            main.LayoutStyle = LayoutStyle.Computed;
+            main.RowOffset = 0;
 
             main.X = 0;
             main.Y = 0;
-            main.Width = Dim.Fill();
-            main.Height = Dim.Fill();
-            main.SetChildNeedsDisplay();
 
-            this.Add(sec);
+            main.Width = Dim.Fill();
+            main.Height = Dim.Fill() - 1;
+
+            main.Table = dt;
+
+            LineView line = new LineView(Orientation.Horizontal)
+            {
+                X = 0,
+                Y = Pos.Bottom(main),
+                Width = Dim.Fill(),
+                Height = 1,
+                StartingAnchor = '└',
+                EndingAnchor = '┘',
+            };
+
+            LineView lineLeft = new LineView(Orientation.Vertical)
+            {
+                X = 0,
+                Y = 0,
+                Width = 1,
+                Height = Dim.Fill(),
+                StartingAnchor = '┌',
+                EndingAnchor = '└',
+            };
+
+            LineView lineRight = new LineView(Orientation.Vertical)
+            {
+                X = Pos.Right(main) - 1,
+                Y = 0,
+                Width = 1,
+                Height = Dim.Fill(),
+                StartingAnchor = '┐',
+                EndingAnchor = '┘',
+            };
 
             this.ColorScheme = Program.GLOBAL_CS;
+            this.AutoSize = true;
+            this.Title = "Klausurenübersicht";
             this.Add(main);
+            this.Add(line);
+            this.Add(lineLeft);
+            this.Add(lineRight);
         }
     }
 }
