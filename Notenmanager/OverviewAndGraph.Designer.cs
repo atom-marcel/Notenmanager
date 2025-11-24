5f5cffc12a380b5c6f89587a5fb6e81c60eae2fb
Marcel Maluta <marcelmaluta@gmail.com>
2025-11-24 15:11:48 +0100

    GraphView Experiment

--- Notenmanager/OverviewAndGraph.Designer.cs
+++ Notenmanager/OverviewAndGraph.Designer.cs
@@ -26,18 +26,28 @@ namespace Notenmanager
             mainView.Height = Dim.Fill();
             Add(mainView);
 
+            BarSeries bs = new BarSeries();
+            bs.Bars.Add(new BarSeries.Bar("Test", new GraphCellToRender('X'), 50));
+            bs.Bars.Add(new BarSeries.Bar("Test2", new GraphCellToRender('X'), 75));
+            bs.Orientation = Orientation.Vertical;
+            bs.Offset = 0;
+            bs.BarEvery = 1;
+            bs.DrawLabels = true;
+
             secView = new GraphView();
+
             secView.X = Pos.Right(mainView) + 1;
             secView.Y = 0;
             secView.Width = Dim.Fill();
             secView.Height = Dim.Fill();
-            secView.AxisX.Minimum = 1;
 
-            HorizontalAxis xAxis = new HorizontalAxis();
-            xAxis.Minimum = 1;
-            xAxis.DrawAxisLine(secView);
+            secView.CellSize = new PointF(1, 0.1f);
+            bs.DrawSeries(secView, secView.Bounds, new RectangleF(0,0, 5, 100));
 
+            
             Add(secView);
+
+            secView.Redraw(secView.Bounds);
 
             this.Title = "Strg-Q drücken um zum Hauptmenü zu gelangen";
             this.ColorScheme = Program.GLOBAL_CS;
